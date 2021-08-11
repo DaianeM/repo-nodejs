a43f5573a737b479136e24cb772ab5922f695a58
DaianeM <daianersm2@gmail.com>
2021-08-11 12:05:16 -0300

    server http

---
+++ api-spoilers/src/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 8080;
+const url = `http://${host}:${port}/`;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World.');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server listening at ${url}`);
+});
