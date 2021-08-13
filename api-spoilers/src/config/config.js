9cf9088dccb4ac48022101631c6bf76524c7c370
DaianeM <daianersm2@gmail.com>
2021-08-13 09:59:44 -0300

    Express and Pug installed

---
+++ api-spoilers/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    development: {
+        database: {
+            host: 'localhost',
+            port: 3306,
+            
+
+        }
+    },
+    production: {
+
+    }
+}
