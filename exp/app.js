f2bd11d1972ded67be06aa4185780f3df649b139
DaianeM <daianersm2@gmail.com>
2021-08-16 10:39:41 -0300

    cookieParser e tratamento de erros

--- exp/app.js
+++ exp/app.js
@@ -3,13 +3,33 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
+const cookieParser = require('cookie-parser');
 
 //exemplo de middleware do express para aceitar req tipo json
 app.use(express.json());
+//acesso aos recursos de cookie - (função de middleware de terceiros)
+app.use(cookieParser());
 
 app.use((req, res, next)=>{
     console.log('Executando middleware em nível de aplicação');
     return next();
+});
+
+//gravando um cookie na máquina do usuário 
+app.get('/setcookie', (req, res)=>{
+    res.cookie('user', 'Daiane', {
+        maxAge: 900000,
+        httpOnly: true
+    });
+
+    res.send('Cookie gravado com sucesso.');
+});
+//recuperando o cookie
+app.get('/getcookie', (req, res)=>{
+    const user = req.cookies['user']
+    if(user){
+        res.json({user});
+    }
 });
 
 //exemplo de criação de uma funcao de middleware
@@ -18,12 +38,19 @@ const logReq = (req, res, next) => {
     return next();
 }
 
+// Rotas
 app.get('/', (req, res) => {
     res.send('|| Bem vindo à aplicação ||');
 });
 
 app.use('/admin', adminRoutes);
 app.use('/user', logReq, userRoutes);
+
+//função de middleware para tratamento de erros
+app.use((err, req, res, next)=>{
+    console.log(err.stack);
+    res.status(500).json({message: err.message});
+});
 
 app.listen(3000, () => {
     console.log(`Server listening at http://localhost:3000`);
